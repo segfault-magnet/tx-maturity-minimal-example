3b9f40348cf36b579af0ee5f97d459c98c28d8ef
Ahmed Sagdati <ahmed.sagdati.ets@gmail.com>
2022-06-29 13:45:18 +0200

    add minimal example of maturity issue with contract deployment

--- tests/harness.rs
+++ tests/harness.rs
@@ -1,39 +1,73 @@
 use fuels::{prelude::*};
-use fuels::client::FuelClient;
 use fuels_abigen_macro::abigen;
+use crate::Error::TransactionError;
 
 // Load abi from json
 abigen!(MyContract, "out/debug/maturity_minimal_example-abi.json");
 
 #[tokio::test]
-async fn can_get_contract_id() {
-    // Launch a local network and deploy the contract
-    let wallet = launch_provider_and_get_single_wallet().await;
+async fn calling_a_contract_function_respects_maturity() -> Result<(), Error> {
+    let wallet = launch_provider_and_get_wallet().await;
 
-    let client = wallet.get_provider().unwrap().client.clone();
+    let provider = wallet.get_provider().unwrap().clone();
 
-    assert_eq!(latest_block_height(&client).await, 0);
+    assert_eq!(provider.latest_block_height().await?, 0);
 
     let id = Contract::deploy("./out/debug/maturity_minimal_example.bin", &wallet, TxParameters::default())
         .await
         .unwrap();
 
-    assert_eq!(latest_block_height(&client).await, 1);
-
     let instance = MyContract::new(id.to_string(), wallet);
 
-    instance.test_function().call().await.unwrap();
+    let call_contract_using_maturity = |maturity| {
+        let mut prepared_call = instance.test_function();
+        prepared_call.tx_parameters.maturity = maturity;
 
-    assert_eq!(latest_block_height(&client).await, 2);
+        prepared_call.call()
+    };
 
-    let mut call_w_maturity = instance.test_function();
-    call_w_maturity.tx_parameters.maturity = 1;
 
-    // 'Contract call error: Response errors; unexpected block execution error
-    // VmExecution { error: ValidationError(TransactionMaturity),
-    call_w_maturity.call().await.unwrap();
+    assert_eq!(provider.latest_block_height().await?, 1);
+    call_contract_using_maturity(2).await.err().expect("Should have failed since we're requiring a maturity greater than the current block height");
+
+    assert_eq!(provider.latest_block_height().await?, 1);
+    call_contract_using_maturity(1).await.expect("This should now pass since we're using a maturity <= current block height");
+
+    Ok(())
 }
 
-async fn latest_block_height(client: &FuelClient) -> u64 {
-    client.chain_info().await.unwrap().latest_block.height.0
+#[tokio::test]
+async fn deploying_a_contract_respects_maturity() -> anyhow::Result<()> {
+    let wallet = launch_provider_and_get_wallet().await;
+
+    let provider = wallet.get_provider().unwrap().clone();
+
+    let deploy_the_contract_w_maturity = |maturity| {
+        let params = TxParameters {
+            maturity,
+            ..TxParameters::default()
+        };
+        Contract::deploy("./out/debug/maturity_minimal_example.bin", &wallet, params)
+    };
+
+    // Only the genesis block present
+    assert_eq!(provider.latest_block_height().await?, 0);
+    let err = deploy_the_contract_w_maturity(1).await.err().expect("Should have failed because we're calling with maturity 1 at block height 0");
+    assert!(is_transaction_maturity_error(&err));
+
+    // The block height remains unchanged
+    assert_eq!(provider.latest_block_height().await?, 0);
+
+    // We're now adding a block by executing a noop script via `add_blocks`.
+    add_blocks(&wallet, 1).await?;
+
+    assert_eq!(provider.latest_block_height().await?, 1);
+    // this fails with execution error: PredicateFailure
+    deploy_the_contract_w_maturity(1).await.expect("This should have passed since we're now deploying with maturity == current block height");
+
+    Ok(())
+}
+
+fn is_transaction_maturity_error(err: &Error) -> bool {
+    matches!(err, TransactionError(msg) if msg.contains("TransactionMaturity"))
 }
